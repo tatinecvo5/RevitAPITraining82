4a0c6826592353bfa95d5cb12f775604b57c2dd8
dom31 <dom31@LAPTOP-19N5M1CV>
2022-12-05 23:27:12 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITraining82/Main.cs
@@ -0,0 +1,41 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITraining82
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+        //public class NavisworksExportOptions : IDisposable
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            /*return Execute(commandData, ref message, elements, NavisworksExportOptions);
+        }
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements, NavisworksExportOptions navisworksExportOptions)
+        {*/
+            //public void Export(string folder, string name, NavisworksExportOptions options)
+
+
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            using (var ts = new Transaction(doc, "экспорт в NWC"))
+            {
+                ts.Start();
+                var nwcOption = new NavisworksExportOptions();
+                doc.Export(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "файл.nwc", nwcOption);
+
+                ts.Commit();
+            }
+            TaskDialog.Show("Cообщение", "Файл NWC сохранен на рабочем столе");
+            return Result.Succeeded;
+        }
+    }
+
+}
